24b4ee204111a5b79d06d03b7a028a45f16823ef
radec240788 <93542168+radec240788@users.noreply.github.com>
2021-11-26 18:08:51 -0600

    Privacy

---
+++ privacy.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    include 'head.php';
+?>
+
+<body>
+ 
+<div class="wrapper">
+
+<!--=================================
+ preloader -->
+ 
+<div id="pre-loader">
+    <img src="images/pre-loader/loader-01.svg" alt="">
+</div>
+
+<!--=================================
+ preloader -->
+
+<!--=================================
+ header -->
+
+<?php
+    include 'menu.php';
+?>
+
+<!--=================================
+ header -->
+
+
+<!--=================================
+page-title-->
+
+<section class="page-title bg-overlay-black-60 parallax" data-jarallax='{"speed": 0.6}' style="background-image: url(images/bg/05.jpg);">
+  <div class="container">
+    <div class="row"> 
+      <div class="col-lg-12"> 
+      <div class="page-title-name">
+          <h1>Política de privacidad</h1>
+          <p>Protegemos tus datos, tanto como si fueran los nuestros.</p>
+        </div>
+          <ul class="page-breadcrumb">
+            <li><a href="./"><i class="fa fa-home"></i> Inicio</a> <i class="fa fa-angle-double-right"></i></li>
+            <li><a href="#"><span>Política de privacidad</a></span> </li>
+       </ul>
+     </div>
+   </div>
+  </div>
+</section>
+
+<!--=================================
+page-title -->
+
+
+<!--=================================
+ sitemap-->
+
+<section class="privacy-policy page-section-ptb">
+  <div class="container">
+    <div class="row">
+     <div class="col-lg-12 col-md-12">
+      <p>Quidem error quae illo excepturi nostrum blanditiis laboriosam magnam explicabo. lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+      <p>Eum nihil expedita dolorum odio dolorem, explicabo rem illum magni perferendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem error quae illo excepturi nostrum blanditiis laboriosam magnam explicabo. Molestias, eum nihil expedita dolorum odio dolorem, explicabo rem illum magni perferendis.</p>
+      <p>Laboriosam magnam explicabo lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem error quae illo excepturi nostrum blanditiis. Molestias, eum nihil expedita dolorum odio dolorem, explicabo rem illum magni perferendis.</p>
+      <h4 class="mt-30 theme-color">Personal Information</h4>
+      <ul class="list list-unstyled">
+        <li> <i class="fa fa-angle-right"></i> Amet consectetur lorem ipsum dolor sit  </li>
+        <li> <i class="fa fa-angle-right"></i> Quidem error quae illo excepturi nostrum blanditiis laboriosam </li>
+        <li> <i class="fa fa-angle-right"></i> Molestias, eum nihil expedita dolorum odio dolorem</li>
+        <li> <i class="fa fa-angle-right"></i> Eum nihil expedita dolorum odio dolorem</li>
+        <li> <i class="fa fa-angle-right"></i> Explicabo rem illum magni perferendis</li>
+      </ul>
+      <p class="mt-15">Debitis nemo animi quia deleniti commodi nesciunt illo. Deserunt. lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, ex, quisquam. Nulla excepturi sint iusto incidunt sed omnis expedita, commodi dolores. </p>
+      <h4 class="mt-30 theme-color">Use of User Information.</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, ex, quisquam. Nulla excepturi sint iusto incidunt sed omnis expedita, commodi dolores. Debitis nemo animi quia deleniti commodi nesciunt illo. Deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, ex, quisquam. Nulla excepturi sint iusto incidunt sed omnis expedita, commodi dolores. Debitis nemo animi quia deleniti commodi nesciunt illo. Deserunt.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, ex, quisquam. Nulla excepturi sint iusto incidunt sed omnis expedita, commodi dolores. Debitis nemo animi quia deleniti commodi nesciunt illo. Deserunt.</p>
+      <h4 class="mt-30 theme-color">Disclosure of User Information.</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem ullam nostrum dolor alias aspernatur nobis suscipit eaque cumque distinctio eos, beatae deserunt, nihil nam maiores vero, eius harum. Reprehenderit, aspernatur.<a href="#"> support@Webster.com</a> </p>
+      <ul class="list list-unstyled">
+        <li> <i class="fa fa-angle-right"></i> Nulla excepturi sint iusto incidunt sed omnis expedita </li>
+        <li> <i class="fa fa-angle-right"></i> Quidem error quae illo excepturi nostrum blanditiis laboriosam </li>
+        <li> <i class="fa fa-angle-right"></i> Deserunt.Lorem ipsum dolor sit amet</li>
+        <li> <i class="fa fa-angle-right"></i> Possimus, ex, quisquam. Nulla excepturi</li>
+      </ul>
+      <p class="mt-15 mb-0">Commodi nesciunt illo. Deserunt lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus, ex, quisquam. Nulla excepturi sint iusto incidunt sed omnis expedita, commodi dolores. Debitis nemo animi quia deleniti .<a href="#"> support@Webster.com</a></p>
+     </div> 
+    </div>
+  </div>
+</section>
+
+<!--=================================
+ sitemap-->
+     
+
+<!--=================================
+action box- -->
+
+<section class="action-box theme-bg full-width">
+  <div class="container">
+    <div class="row">
+     <div class="col-lg-12 col-md-12">
+       <div class="action-box-text">
+        <h3><strong> Radec design: </strong> Las plantillas de calidad que buscabas</h3>
+        <p>Tratamos que tus esfuerzos se vean plasmados tal como lo imaginas y nosotros nos encargamos que pasen a online.</p>
+      </div>
+      <div class="action-box-button">
+        <a class="button button-border white" href="#">
+          <span>Cotiza ahora</span>
+          <i class="fa fa-download"></i>
+       </a> 
+     </div>
+    </div>
+  </div>
+ </div>
+</section>
+ 
+<!--=================================
+action box- -->
+ 
+ 
+<!--=================================
+ footer -->
+ 
+<footer class="footer page-section-pt black-bg">
+ <div class="container">
+  <div class="row">
+      <div class="col-lg-2 col-sm-6 sm-mb-30">
+      <div class="footer-useful-link footer-hedding">
+        <h6 class="text-white mb-30 mt-10 text-uppercase">Navigation</h6>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Service</a></li>
+          <li><a href="#">Team</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-lg-2 col-sm-6 sm-mb-30">
+      <div class="footer-useful-link footer-hedding">
+        <h6 class="text-white mb-30 mt-10 text-uppercase">Useful Link</h6>
+        <ul>
+          <li><a href="#">Create Account</a></li>
+          <li><a href="#">Company Philosophy</a></li>
+          <li><a href="#">Corporate Culture</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Client Management</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-lg-4 col-sm-6 xs-mb-30">
+    <h6 class="text-white mb-30 mt-10 text-uppercase">Contact Us</h6>
+    <ul class="addresss-info"> 
+        <li><i class="fa fa-map-marker"></i> <p>Address: 17504 Carlton Cuevas Rd, Gulfport, MS, 39503</p> </li>
+        <li><i class="fa fa-phone"></i> <a href="tel:7042791249"> <span>+(704) 279-1249 </span> </a> </li>
+        <li><i class="fa fa-envelope-o"></i>Email: letstalk@webster.com</li>
+      </ul>
+    </div>
+    <div class="col-lg-4 col-sm-6">
+      <h6 class="text-white mb-30 mt-10 text-uppercase">Subscribe to Our Newsletter</h6>
+        <p class="mb-30">Sign Up to our Newsletter to get the latest news and offers.</p>
+         <div class="footer-Newsletter">
+          <div id="mc_embed_signup_scroll">
+            <form action="php/mailchimp-action.php" method="POST" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate">
+             <div id="msg"> </div>                 
+              <div id="mc_embed_signup_scroll_2">
+                <input id="mce-EMAIL" class="form-control" type="text" placeholder="Email address" name="email1" value="">
+              </div>
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                    <input type="text" name="b_b7ef45306f8b17781aa5ae58a_6b09f39a55" tabindex="-1" value="">
+                </div>
+                <div class="clear">
+                  <button type="submit" name="submitbtn" id="mc-embedded-subscribe" class="button button-border mt-20 form-button">  Get notified </button>
+                </div>
+              </form>
+            </div>
+            </div>
+         </div>
+       </div>
+      <div class="footer-widget mt-20">
+        <div class="row">
+          <div class="col-lg-6 col-md-6">
+           <p class="mt-15"> &copy;Copyright <span id="copyright"> <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script></span> <a href="#"> Webster </a> All Rights Reserved </p>
+          </div>
+          <div class="col-lg-6 col-md-6 text-left text-md-right">
+            <div class="social-icons color-hover mt-10">
+             <ul> 
+              <li class="social-facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li class="social-twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li class="social-dribbble"><a href="#"><i class="fa fa-dribbble"></i> </a></li>
+              <li class="social-twitter"><a href="#"><i class="fa fa-twitter"></i> </a></li>
+             </ul>
+           </div>
+          </div>
+        </div>    
+      </div>
+  </div>
+</footer>
+
+<!--=================================
+ footer -->
+
+</div>
+
+  
+
+<div id="back-to-top"><a class="top arrow" href="#top"><i class="fa fa-angle-up"></i> <span>TOP</span></a></div>
+ 
+<!--=================================
+ jquery -->
+
+<!-- jquery -->
+<script src="js/jquery-3.3.1.min.js"></script>
+
+<!-- plugins-jquery -->
+<script src="js/plugins-jquery.js"></script>
+
+<!-- plugin_path -->
+<script>var plugin_path = 'js/';</script>
+ 
+<!-- custom -->
+<script src="js/custom.js"></script>
+
+ 
+ 
+</body>
+</html>
+
