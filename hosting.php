6553accb7493977959b417c5a3ef131b8f83023d
radec240788 <93542168+radec240788@users.noreply.github.com>
2021-12-13 18:58:32 -0600

    Hosting service

---
+++ hosting.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+    include 'head.php';
+?>
+
+<body>
+ 
+<div class="wrapper">
+
+<!--=================================
+ preloader -->
+ 
+<div id="pre-loader">
+    <img src="images/pre-loader/loader-01.svg" alt="">
+</div>
+
+<!--=================================
+ preloader -->
+
+<!--=================================
+ header -->
+
+<?php
+    include 'menu.php';
+?>
+
+<!--=================================
+ header -->
+
+<!--=================================
+page-title-->
+
+<section class="page-title bg-overlay-black-60 parallax" data-jarallax='{"speed": 0.6}' data-jarallax-video="https://www.youtube.com/watch?v=Nv7RgGpu6ME">
+  <div class="container">
+    <div class="row"> 
+      <div class="col-lg-12"> 
+      <div class="page-title-name">
+          <h1>Hosting</h1>
+          <p>Los mejores planes de acuerdo a tus necesidades.</p>
+        </div>
+          <ul class="page-breadcrumb">
+            <li><a href="#"><i class="fa fa-home"></i> Inicio</a> <i class="fa fa-angle-double-right"></i></li>
+            <li><span>hosting</span> </li>
+       </ul>
+     </div>
+   </div>
+  </div>
+</section>
+
+<!--=================================
+page-title -->
+ 
+
+<!--=================================
+ service-->
+
+<section class="service white-bg page-section-ptb" style="padding-bottom: 20px;">
+  <div class="container">
+    <div class="row justify-content-center">
+       <div class="col-lg-8">
+        <div class="section-title text-center">
+            <h2 class="title-effect">Hosting rápido y seguro</h2>
+            <p>Mantener su sitio web 100% de el tiempo disponible y con un alto rendimiento hará que sus clientes naveguen con comodidad y confianza</p>
+          </div>
+
+
+      </div>
+    </div>
+<!-- =========================================== -->
+ <div class="row">
+        <div class="col-lg-4 col-md-4 mb-30">
+          <div class="feature-text box-shadow h-100 text-center">
+            <div class="feature-icon">
+              <span class="ti-layers-alt theme-color"></span>
+            </div>
+            <div class="feature-info">
+              <h4 class="pb-10">Administración remota</h4>
+              <p>Olvídese de requerimientos técnicos y ahorre valioso tiempo poniendo en nuestras manos la administración de su sitio.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-4 mb-30">
+          <div class="feature-text box-shadow h-100 text-center">
+            <div class="feature-icon">
+            <span aria-hidden="true" class="ti-split-v theme-color"></span>
+            </div>
+            <div class="feature-info">
+              <h4 class="pb-10">Alertas</h4>
+              <p>Le enviaremos alertas cuando su espacio este por agotarse o su próxima fecha de renovación. Brindándole opciones para mantener su sitio en línea y evitar perdida de información.</p>
+           </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-4 mb-30">
+          <div class="feature-text box-shadow h-100 text-center">
+            <div class="feature-icon">
+            <span aria-hidden="true" class="ti-heart theme-color"></span>
+            </div>
+            <div class="feature-info">
+            <h4 class="pb-10">Respaldos</h4>
+            <p>En la mayoría de nuestros paquetes ofrecemos respaldos de sus sitio y/o bases de datos. De esta manera salvaguardamos su información.</p>
+          </div>
+          </div>
+        </div>
+     </div>
+   </div>
+</section>
+
+<!--=================================
+ service-->
+
+ <!--=================================
+ Services Details -->
+
+<section class="page-section-ptb" style="padding-bottom: 40px;">
+        <div class="container">
+            <div class="row justify-content-center">
+             <div class="col-lg-8">
+                 <div class="section-title mb-60 text-center">
+                  <h2 class="title-effect">Precios accesibles</h2>
+                  <p>Ofrecemos precios competentes a lo que hoy en día la mayoría ofrece.</p>
+                 </div>
+            </div>
+            </div>
+                 <div class="row">
+                <div class="col-lg-4 col-md-4">
+                   <div class="pricing-table boxed">
+                     <div class="pricing-top">
+                       <div class="pricing-title">
+                         <h3 class="mb-15">WP Live</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, magni!</p>
+                       </div>
+                       <div class="pricing-prize">
+                         <h2>$49 /<span> month</span> </h2>
+                       </div>
+                       <div class="pricing-button">
+                         <a class="button" href="#">Sign Up</a>
+                       </div>
+                     </div>
+                     <div class="pricing-content">
+                       <div class="pricing-table-list">
+                            <ul class="list-unstyled">
+                                <li> <i class="fa fa-check"></i> 25 Analytics Campaign <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span></li>
+                                <li><i class="fa fa-times"></i> Branded Reports <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li><i class="fa fa-check"></i> 1,900 Keywords <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li> <i class="fa fa-times"></i> 4 Social Account <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li> <i class="fa fa-times"></i>  Phone & Email Support <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                            </ul>
+                        </div>
+                     </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-4">
+                    <div class="pricing-table boxed active">
+                     <div class="pricing-top">
+                       <div class="pricing-title">
+                         <h3 class="mb-15">WP Live</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, magni!</p>
+                       </div>
+                       <div class="pricing-prize">
+                         <h2>$129 /<span> month</span> </h2>
+                       </div>
+                       <div class="pricing-button">
+                         <a class="button" href="#">Sign Up</a>
+                       </div>
+                     </div>
+                     <div class="pricing-content">
+                       <div class="pricing-table-list">
+                            <ul class="list-unstyled">
+                                <li> <i class="fa fa-check"></i> 25 Analytics Campaign <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span></li>
+                                <li><i class="fa fa-check"></i> Branded Reports <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li><i class="fa fa-check"></i> 1,900 Keywords <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li> <i class="fa fa-check"></i> 4 Social Account <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li> <i class="fa fa-check"></i>  Phone & Email Support <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                            </ul>
+                        </div>
+                     </div>
+                  </div> 
+                </div>
+                <div class="col-lg-4 col-md-4">
+                   <div class="pricing-table boxed">
+                     <div class="pricing-top">
+                       <div class="pricing-title">
+                         <h3 class="mb-15">WP Live</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur, magni!</p>
+                       </div>
+                       <div class="pricing-prize">
+                         <h2>$99 /<span> month</span> </h2>
+                       </div>
+                       <div class="pricing-button">
+                         <a class="button" href="#">Sign Up</a>
+                       </div>
+                     </div>
+                     <div class="pricing-content">
+                       <div class="pricing-table-list">
+                            <ul class="list-unstyled">
+                                <li> <i class="fa fa-check"></i> 25 Analytics Campaign <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span></li>
+                                <li><i class="fa fa-times"></i> Branded Reports <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li><i class="fa fa-check"></i> 1,900 Keywords <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li> <i class="fa fa-times"></i> 4 Social Account <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                                <li> <i class="fa fa-check"></i>  Phone & Email Support <span class="tooltip-content float-right" data-placement="top" data-toggle="tooltip" data-original-title="Lorem ipsum dolor sit amet"><i class="fa fa-info"></i></span> </li>
+                            </ul>
+                        </div>
+                     </div>
+                  </div> 
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!--=================================
+ Services Details-->
+
+<!--=================================
+ footer -->
+ 
+<footer class="footer footer-one-page page-section-pt black-bg">
+ <div class="container">
+  <div class="row text-center">
+   <div class="col-lg-4 col-md-4">
+      <div class="contact-add mb-30"> 
+       <div class="text-center">
+           <i class="ti-map-alt text-white"></i>
+           <h5 class="mt-15">Dirección</h5>
+           <p>Cuahutémoc 63, Ciudad de México, 09200</p>
+          </div>
+      </div>
+   </div>
+   <div class="col-lg-4 col-md-4">
+      <div class="contact-add mb-30"> 
+       <div class="text-center">
+         <i class="ti-mobile text-white"></i>
+         <h5 class="mt-15">Llámanos</h5>
+         <p>55 4906 9544</p>
+        </div>
+      </div>
+   </div>
+   <div class="col-lg-4 col-md-4">
+      <div class="contact-add mb-30"> 
+        <div class="text-center">
+         <i class="ti-email text-white"></i>
+         <h5 class="mt-15">Envíanos un email</h5>
+         <p>ventas@radec-design.com.mx</p>
+       </div>
+      </div>
+   </div>
+   </div>
+    <div class="row justify-content-center">
+    <div class="col-lg-4 col-md-7">
+       <div class="footer-Newsletter text-center mt-30 mb-40">
+          <div id="mc_embed_signup_scroll">
+            <form action="php/mailchimp-action.php" method="POST" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate">
+             <div id="msg"> </div>                 
+              <div id="mc_embed_signup_scroll_2">
+                <input id="mce-EMAIL" class="form-control" type="text" placeholder="Correo electrónico" name="email1" value="">
+              </div>
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                    <input type="text" name="b_b7ef45306f8b17781aa5ae58a_6b09f39a55" tabindex="-1" value="">
+                </div>
+                <div class="clear">
+                  <button type="submit" name="submitbtn" id="mc-embedded-subscribe" class="button button-border mt-20 form-button">  Envíanos un email </button>
+                </div>
+              </form>
+            </div>
+            </div>
+     </div>
+    </div>
+   <div class="footer-widget mt-20">
+    <div class="row">
+      <div class="col-lg-6 col-md-6">
+      <p class="mt-15">&copy;Copyright <span id="copyright"> <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script></span> <a href="#"> Radec Design </a> All Rights Reserved</p>
+      </div>
+      <div class="col-lg-6 col-md-6 ">
+        <div class="footer-widget-social text-center text-md-right">
+         <ul> 
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-dribbble"></i> </a></li>
+          <li><a href="#"><i class="fa fa-linkedin"></i> </a></li>
+         </ul>
+       </div>
+      </div>
+    </div>    
+  </div>
+  </div>
+</footer>
+
+<!--=================================
+ footer -->
+ 
+</div>
+
+  
+<div id="_evidon_banner" class="evidon-banner animate-start" role="dialog" aria-label="Cookie notice banner" style="width: 100%; display: none; flex: 1 1 0%; background: rgb(247, 245, 242); font-size: 14px; color: rgb(51, 51, 51); padding: 24px 48px; z-index: 10001; box-sizing: border-box; font-family: &quot;Atlas Grotesk Web&quot;, &quot;Atlas Grotesk&quot;, AtlasGrotesk, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;; position: fixed; bottom: 0px; text-size-adjust: 100%; visibility: visible;"><div id="_evidon-message" class="evidon-banner-message" style="margin-right: 48px; margin-left: 60px; color: rgb(30, 25, 25); font-size: 12px; font-weight: 400; line-height: 16px; text-align: left; width: 70%; align-self: center;">Utilizamos cookies con el fin de ofrecer, mejorar, proteger y promocionar nuestros servicios. Para obtener más información, visita nuestra <a href="https://www.dropbox.com/privacy" target="_blank" rel="noopener noreferrer">Política de privacidad</a> y nuestro <a href="/privacy" target="_blank" rel="noopener noreferrer">Aviso de privacidad</a>. Puedes cambiar tus preferencias enviando un correo a soporte@radec-design.com.mx</div><button id="_evidon-decline-button" class="evidon-banner-declinebutton" tabindex="0" style="display: flex; flex-flow: column nowrap; flex-shrink: 0; justify-content: center; align-self: center; border: 1px solid rgb(30, 25, 25); background: rgb(247, 245, 242); color: rgb(30, 25, 25); padding: 9px 20px; cursor: pointer; font-size: 16px; line-height: 26px; order: 2; font-family: &quot;Atlas Grotesk Web&quot;, &quot;Atlas Grotesk&quot;, AtlasGrotesk, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;;">Rechazar</button><button id="_evidon-accept-button" class="evidon-banner-acceptbutton" tabindex="0" aria-label="Aceptar" style="display: flex; flex-flow: column nowrap; justify-content: center; align-self: center; flex-shrink: 0; border: 1px solid rgb(30, 25, 25); color: rgb(250, 250, 250); background: rgb(30, 25, 25); padding: 9px 20px; cursor: pointer; margin: 0px 16px; font-size: 16px; line-height: 26px; order: 3; font-family: &quot;Atlas Grotesk Web&quot;, &quot;Atlas Grotesk&quot;, AtlasGrotesk, -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;;">Aceptar</button></div>
+
+<div id="back-to-top"><a class="top arrow" href="#top"><i class="fa fa-angle-up"></i> <span>Inicio</span></a></div>
+ 
+<!--=================================
+ jquery -->
+
+<!-- jquery -->
+<script src="js/jquery-3.3.1.min.js"></script>
+
+<!-- plugins-jquery -->
+<script src="js/plugins-jquery.js"></script>
+
+<!-- plugin_path -->
+<script>var plugin_path = 'js/';</script>
+ 
+<!-- REVOLUTION JS FILES -->
+<script src="revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+<script src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+<!-- revolution custom --> 
+<script src="revolution/js/revolution-custom.js"></script> 
+
+<!-- custom -->
+<script src="js/custom.js"></script>
+<script src="js/actions.js"></script>
+
+<!-- Código de instalación Cliengo para Radec Design --> 
+<script type="text/javascript">(function () { 
+    var ldk = document.createElement('script'); ldk.type = 'text/javascript'; ldk.async = true; 
+    ldk.src = 'https://s.cliengo.com/weboptimizer/618416fa3d689f002a8cc05e/618416fd3d689f002a8cc063.js?platform=view_installation_code'; 
+    var s = document.getElementsByTagName('script')[0]; 
+    s.parentNode.insertBefore(ldk, s); })();
+</script>
+
+</body>
+</html>
