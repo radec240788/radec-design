57ef9c31af38bdbd880e9b541f6855b754da0860
radec240788 <93542168+radec240788@users.noreply.github.com>
2022-04-25 10:47:29 -0500

    Remove logo

--- vinopremier/index.php
+++ vinopremier/index.php
@@ -52,9 +52,6 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="./index.html">
-                            <img src="img/nav-logo.png" style="width: 30%;" alt="nav-logo">
-                        </a>
                     </div>
                     <div id="navbar" class="navbar-collapse collapse">
                         <ul class="nav navbar-nav navbar-right">
